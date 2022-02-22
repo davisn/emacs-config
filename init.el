503f8a8cce81171f1b233385cd622ab89230a69e
Nathan Davis <ndavis@positronic-solutions.com>
2022-02-21 19:20:09 -0600

    Remove color theme
    * Obsolete and broken in current emacs versions

--- init.el
+++ init.el
@@ -1,7 +1,6 @@
 ;;(load "~/emacs/slime-init")
 (load "~/emacs/package-utils")
 (load "~/emacs/repositories")
-(load "~/emacs/color-theme")
 (load "~/emacs/clojure-init")
 (load "~/emacs/paredit")
 (load "~/emacs/misc")
